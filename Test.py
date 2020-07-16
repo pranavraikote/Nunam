688a847bdd403782a1eaa565e606bf26c9348752
pranavraikote <pranavraikote@gmail.com>
2020-07-17 03:23:00 +0530

    Added test cases for all functions and updated the file Test.py

--- Test.py
+++ Test.py
@@ -14,6 +14,7 @@ class TestNunam(unittest.TestCase):
     
     #Function to test the concat_dataframes function
     def test_concat(self):
+        
         dfz = pd.DataFrame.from_dict({
                 'Record ID': [1, 1],
                 'Gap of Voltage': [0.0, 0.0]
@@ -33,6 +34,7 @@ class TestNunam(unittest.TestCase):
     
     #Function to test the read_and_extract function
     def test_read_and_extract(self):
+        
         b = n.read_and_extract('Book1.xls', 'Sheet1')
         
         output = pd.DataFrame.from_dict({
@@ -44,7 +46,21 @@ class TestNunam(unittest.TestCase):
             })
         
         pd.testing.assert_frame_equal(b, output)
-
+        
+    def test_sampling(self):
+        
+        a = n.read_and_extract('Book2.xls', 'Sheet1')
+        b = n.resample_data(a, 'Realtime', ['Record ID'])
+        
+        output = pd.DataFrame.from_dict({
+            'Realtime': ['2019-11-15 19:38:00'],
+            'Auxiliary channel TU1 U(V)': [-0.004436],
+            'Gap of Voltage': [0]
+            })
+        
+        output.set_index(pd.DatetimeIndex(output['Realtime']), inplace = True)
+        
+        pd.testing.assert_frame_equal(b, output, check_names=False, check_index_type = False)
     
 if __name__ == '__main__':
     #Instantiating the Nunam class for accessing the functions
